896ee5e33646334dece49246e20c4fc042699ce9
Suraj Kumar <surajz+github@gmail.com>
2023-08-26 22:34:05 -0400

    done

--- coolcode.java
+++ coolcode.java
@@ -6,6 +6,6 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Constants {
 
-  public static final String password = "mISydD0En55Fq8FXbUfX72K08Vc6/aQYtkFmkp7ntsM=";
+  public static final String password = "mISydDE0n55Fq8FXbUfX72K08Vc6/aQYtkFmkp7ntsM=";
   public static final String newKey = "mISydD0En55Fq8FXbUfX720K8Vc6/aQYtkFmkp7ntsM=";
 }
