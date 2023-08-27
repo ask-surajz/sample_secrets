c821f4353060f540b12f237a90af5371a68e1a3e
Suraj Kumar <surajz+github@gmail.com>
2023-08-26 22:16:19 -0400

    updated java

--- package com.surajz.coolcode.java
+++ package com.surajz.coolcode.java
@@ -6,6 +6,6 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Constants {
 
-  public static final String password = "6w)rx*R'&,!5g36?c^y{j5$#ZZj!ME!";
+  public static final String password = "6w)rMx*R'&,!5g36?c^y{j5$#ZZj!ME!";
   public static final String newKey = "mISydD0En55Fq8FXbUfX720K8Vc6/aQYtkFmkp7ntsM=";
 }
