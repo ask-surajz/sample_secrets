ee659cadc63f8a4fd60590b6f626985705eec28a
Suraj Kumar <surajz+github@gmail.com>
2023-08-26 22:02:25 -0400

    new java code

---
+++ package com.surajz.coolcode.java
@@ -0,0 +1,11 @@
+package com.surajz.coolcode;
+
+import lombok.experimental.UtilityClass;
+
+/** used for Challenges their secrets. */
+@UtilityClass
+public class Constants {
+
+  public static final String password = "6w)rx*R'&,!5g36?c^y{j5$#ZZj!ME!";
+  public static final String newKey = "mISydD0En55Fq8FXbUfX720K8Vc6/aQYtkFmkp7ntsM=";
+}
